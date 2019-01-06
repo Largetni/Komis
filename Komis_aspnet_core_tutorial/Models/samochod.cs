e6f25fe26cc5de57458cfc3ac53482f77fbc78cb
Jakub Slominski <u002750@gmail.com>
2019-01-06 15:25:33 +0100

    fixed typos

--- Komis_aspnet_core_tutorial/Models/samochod.cs
+++ Komis_aspnet_core_tutorial/Models/samochod.cs
@@ -13,13 +13,13 @@ namespace Komis_aspnet_core_tutorial.Models
         public int RokProdukcji { get; set; }
         public string Przebieg { get; set; }
         public string Pojemnosc { get; set; }
-        public string RodzjPaliwa { get; set; }
+        public string RodzajPaliwa { get; set; }
         public string Moc { get; set; }
         public string Opis { get; set; }
         public decimal Cena { get; set; }
         public string ZdjecieUrl { get; set; }
         public string MiniaturkaUrl { get; set; }
-        public bool JestSamochodemTyogdnia { get; set; }
+        public bool JestSamochodemTygodnia { get; set; }
 
     }
 }
