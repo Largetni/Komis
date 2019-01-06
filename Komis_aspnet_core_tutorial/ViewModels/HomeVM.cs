c77033b845c6a4cbf462bc8c1d07e5c41a624537
Jakub Slominski <u002750@gmail.com>
2019-01-06 16:30:14 +0100

    HomeVM properties added

--- Komis_aspnet_core_tutorial/ViewModels/HomeVM.cs
+++ Komis_aspnet_core_tutorial/ViewModels/HomeVM.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Komis_aspnet_core_tutorial.Models;
 
 namespace Komis_aspnet_core_tutorial.ViewModels
 {
     public class HomeVM
     {
+        public string Tytul { get; set; }
+        public List<Samochod> Samochody { get; set; }
     }
 }
