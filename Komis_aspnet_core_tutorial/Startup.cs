5bdfaddbf813504e1344d594a3f682d375fe042d
Jakub Slominski <u002750@gmail.com>
2019-01-06 15:26:17 +0100

    added repository to dependency

--- Komis_aspnet_core_tutorial/Startup.cs
+++ Komis_aspnet_core_tutorial/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Komis_aspnet_core_tutorial.Models;
 
 namespace Komis_aspnet_core_tutorial
 {
@@ -15,6 +16,7 @@ namespace Komis_aspnet_core_tutorial
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ISamochodRepository, MockSamochodRepository>();
             services.AddMvc();
         }
 
