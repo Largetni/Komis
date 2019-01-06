06224c2f6cc99a85e966a4e0d4cb8d432f4b3719
Jakub Slominski <u002750@gmail.com>
2019-01-06 15:22:42 +0100

    Interface repository added

---
+++ Komis_aspnet_core_tutorial/Models/ISamochodRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Komis_aspnet_core_tutorial.Models
+{
+    public interface ISamochodRepository
+    {
+        IEnumerable<Samochod> PobierzWszystkieSamochody();
+        Samochod PobierzSamochod(int samochodId);
+    }
+}
