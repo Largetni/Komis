51c69a15735fbffe543bdec43492ca6c2c4a6ce3
Jakub Slominski <u002750@gmail.com>
2019-01-06 16:09:40 +0100

    Controler created

--- Komis_aspnet_core_tutorial/Controllers/HomeController.cs
+++ Komis_aspnet_core_tutorial/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Komis_aspnet_core_tutorial.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,20 @@ namespace Komis_aspnet_core_tutorial.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ISamochodRepository _samochodRepository;
+
+        public HomeController(ISamochodRepository samochodRepository)
+        {
+            _samochodRepository = samochodRepository;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Tytul = "Przegląd Samochodwów";
+
+            var samochody = _samochodRepository.PobierzWszystkieSamochody().OrderBy(s => s.Marka);
+
+            return View(samochody);
         }
     }
 }
