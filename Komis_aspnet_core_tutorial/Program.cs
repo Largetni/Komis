96a50a9c854578c653f93784dfa150ddd555fdb9
Jakub Slominski <u002750@gmail.com>
2019-01-08 23:43:26 +0100

    added using statements

--- Komis_aspnet_core_tutorial/Program.cs
+++ Komis_aspnet_core_tutorial/Program.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Komis_aspnet_core_tutorial.Models;
 
 namespace Komis_aspnet_core_tutorial
 {
