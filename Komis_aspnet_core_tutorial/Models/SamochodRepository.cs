0db7f7b8113b7573dec2aa09bf6d431f34efaaf1
Jakub Slominski <u002750@gmail.com>
2019-01-08 23:18:40 +0100

    Interface implemented in place of stub

--- Komis_aspnet_core_tutorial/Models/SamochodRepository.cs
+++ Komis_aspnet_core_tutorial/Models/SamochodRepository.cs
@@ -5,7 +5,23 @@ using System.Threading.Tasks;
 
 namespace Komis_aspnet_core_tutorial.Models
 {
-    public class SamochodRepository
+    public class SamochodRepository : ISamochodRepository
     {
+        private readonly AppDbContext _appDbContext;
+
+        public SamochodRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public Samochod PobierzSamochod(int samochodId)
+        {
+            return _appDbContext.Samochody.FirstOrDefault(s => s.Id == samochodId);
+        }
+
+        public IEnumerable<Samochod> PobierzWszystkieSamochody()
+        {
+            return _appDbContext.Samochody;
+        }
     }
 }
