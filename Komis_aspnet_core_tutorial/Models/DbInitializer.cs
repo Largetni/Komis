90a2c118f97a8656f18365cba3d2c39a4af3d6dc
Jakub Slominski <u002750@gmail.com>
2019-01-08 23:28:40 +0100

    added db initializer class

---
+++ Komis_aspnet_core_tutorial/Models/DbInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Komis_aspnet_core_tutorial.Models
+{
+    public static class DbInitializer
+    {
+        public static void
+    }
+}
