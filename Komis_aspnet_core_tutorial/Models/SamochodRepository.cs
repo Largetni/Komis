5af94a2f8008bbe92351dea272ffee0a1a44b50d
Jakub Slominski <u002750@gmail.com>
2019-01-08 23:04:31 +0100

    Created db context for samochody

---
+++ Komis_aspnet_core_tutorial/Models/SamochodRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Komis_aspnet_core_tutorial.Models
+{
+    public class SamochodRepository
+    {
+    }
+}
