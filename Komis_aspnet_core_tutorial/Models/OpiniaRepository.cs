c32c296e038743271c852c09887dce13d36348e0
Jakub Slominski <u002750@gmail.com>
2019-01-10 22:38:48 +0100

    added user feedback repository

--- Komis_aspnet_core_tutorial/Models/OpiniaRepository.cs
+++ Komis_aspnet_core_tutorial/Models/OpiniaRepository.cs
@@ -5,7 +5,19 @@ using System.Threading.Tasks;
 
 namespace Komis_aspnet_core_tutorial.Models
 {
-    public class OpiniaRepository
+    public class OpiniaRepository : IOpiniaRepository
     {
+        private readonly AppDbContext _appDbContext;
+
+        public OpiniaRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public void DodajOpinie(Opinia opinia)
+        {
+            _appDbContext.Opinie.Add(opinia);
+            _appDbContext.SaveChanges();
+        }
     }
 }
