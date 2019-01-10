4720465b0f722a7a080fd88f9bc9933a3c01f73c
Jakub Slominski <u002750@gmail.com>
2019-01-10 22:41:07 +0100

    added new controller for user feedback

---
+++ Komis_aspnet_core_tutorial/Controllers/OpinieController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Komis_aspnet_core_tutorial.Controllers
+{
+    public class OpinieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
