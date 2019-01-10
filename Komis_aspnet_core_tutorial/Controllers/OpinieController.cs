461e09408c216d3d1f5ab83e469bbf9c855a4c10
Jakub Slominski <u002750@gmail.com>
2019-01-10 23:08:46 +0100

    added dependency to controler for user feedback repository

--- Komis_aspnet_core_tutorial/Controllers/OpinieController.cs
+++ Komis_aspnet_core_tutorial/Controllers/OpinieController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Komis_aspnet_core_tutorial.Models;
 
 namespace Komis_aspnet_core_tutorial.Controllers
 {
     public class OpinieController : Controller
     {
+        private readonly IOpiniaRepository _opiniaRepository;
+
+        public OpinieController(IOpiniaRepository opiniaRepository)
+        {
+            _opiniaRepository = opiniaRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
