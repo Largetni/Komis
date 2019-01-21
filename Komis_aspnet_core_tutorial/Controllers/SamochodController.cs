1fcf4b47c42896e977a73ef91fdcb2d71a945f21
Jakub Slominski <u002750@gmail.com>
2019-01-21 22:27:53 +0100

    added creadte edit file add and delete

--- Komis_aspnet_core_tutorial/Controllers/SamochodController.cs
+++ Komis_aspnet_core_tutorial/Controllers/SamochodController.cs
@@ -46,5 +46,92 @@ namespace Komis_aspnet_core_tutorial.Controllers
             
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Samochod samochod)
+        {
+            if (ModelState.IsValid)
+            {
+                _samochodRepository.DodajSamochod(samochod);
+                return RedirectToAction("Index");
+            }
+            return View(samochod);
+        }
+
+        public IActionResult Edit(int id, string FileName)
+        {
+            var samochod = _samochodRepository.PobierzSamochod(id);
+
+            if (samochod == null)
+                return NotFound();
+
+            if (!string.IsNullOrEmpty(FileName))
+                ViewBag.ImgPath = "/Images/" + FileName;
+            else
+                ViewBag.ImgPath = samochod.ZdjecieUrl;
+
+            return View(samochod);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Samochod samochod)
+        {
+            if (ModelState.IsValid)
+            {
+                _samochodRepository.EdytujSamochod(samochod);
+                return RedirectToAction("Index");
+            }
+            return View(samochod);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var samochod = _samochodRepository.PobierzSamochod(id);
+
+            if (samochod == null)
+                return NotFound();
+
+            return View(samochod);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id, string ZdjecieUrl)
+        {
+            var samochod = _samochodRepository.PobierzSamochod(id);
+            _samochodRepository.UsunSamochod(samochod);
+
+            //usuwanie pliku
+            if (ZdjecieUrl!=null)
+            {
+                var webroot = _env.WebRootPath;
+                var filePath = Path.Combine(webroot.ToString() + ZdjecieUrl);
+                System.IO.File.Delete(filePath);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost("UploadFile")]
+        public async Task<IActionResult> UploadFile(IFormCollection form)
+        {
+            var webroot = _env.WebRootPath;
+            var filePath = Path.Combine(webroot.ToString() + "\\images\\" + form.Files[0].FileName);
+
+            if (form.Files[0].FileName.Length>0)
+            {
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await form.Files[0].CopyToAsync(stream);
+                }
+            }
+
+            if (Convert.ToString(form["Id"]) == string.Empty || Convert.ToString(form["Id"]) == "0")
+                return RedirectToAction(nameof(Create), new { FileName = Convert.ToString(form.Files[0].FileName) });
+
+            return RedirectToAction(nameof(Edit), new { FileName = Convert.ToString(form.Files[0].FileName), id=Convert.ToString(form["Id"]) });
+        }
     }
 }
